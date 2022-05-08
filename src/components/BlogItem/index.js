5d579183f2d1c68f1cf7cc07c7501c07cf36a129
root <koyyadacharan4@gmail.com.exitcleargit config --global user.email koyyadacharan4@gmail.com>
2022-05-08 20:25:04 +0000

    first commit

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const BlogItem = props => {
+  const {detail} = props
+  const {title, description, publishedDate} = detail
+  return (
+    <li className="item-card">
+      <div className="con-paras">
+        <h1>{title}</h1>
+        <p>{publishedDate}</p>
+      </div>
+      <p>{description}</p>
+    </li>
+  )
+}
+
+export default BlogItem
---
+++ src/components/BlogList/index.js
@@ -0,0 +1,15 @@
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+const BlogList = props => {
+  const {details} = props
+  return (
+    <ul className="un-order-list-con">
+      {details.map(each => (
+        <BlogItem detail={each} key={each.id} />
+      ))}
+    </ul>
+  )
+}
+export default BlogList
